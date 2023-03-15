38e96fb7123b78b8ad22e7c291973be650f8c709
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2023-03-15 11:14:01 -0300

    Classe RtcServices criada

---
+++ libs/RtcServices/examples/Services/RtcServices.cpp
@@ -0,0 +1,21 @@
+/*
+  RtcServices.cpp - Biblioteca para enviar e receber sinais do RTC
+  pelo barramento UART.
+  Criado pelo Luis Gustavo, 15 de Março, 2023.
+*/
+
+#include "RtcServices.h"
+
+RtcServices::RtcServices()
+{
+
+    setupRtc();
+}
+
+void RtcServices::setupRtc(void)
+{
+}
+
+void RtcServices::funcionalidade(void)
+{
+}
