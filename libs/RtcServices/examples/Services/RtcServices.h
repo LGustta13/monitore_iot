6b29a931d10c27e2aec0b9f8a0959a7f852a883e
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2023-04-02 19:13:38 -0300

    Atualizando arquivos cpp, h e txt

--- libs/RtcServices/examples/Services/RtcServices.h
+++ libs/RtcServices/examples/Services/RtcServices.h
@@ -2,33 +2,46 @@
   RtcServices.h - Biblioteca para enviar e receber sinais do RTC
   pelo barramento UART.
   Criado pelo Luis Gustavo, 15 de Março, 2023.
+  Atualizado, 02 de Abril, 2023.
 */
 
 #ifndef RTCSERVICES_H
 #define RTCSERVICES_H
 
 #include <Arduino.h>
+#include <RTClib.h>
 
 class RtcServices
 {
 public:
-  /** Descrição
-   * @param -
-   * @return -
-   */
-  RtcServices(void);
-
-  /** Funcao que realiza o setup dos pinos
+  /** Construtor
    * @param - void
    * @return - void
    */
-  void setupRtc(void);
+  RtcServices(void);
 
-  /** Descrição
-   * @param -
-   * @return -
+  /** Acessa barramento i2c e salva data e horário
+   * @param - void
+   * @return - void
    */
-  void funcionalidade(void);
+  void handleDataHorarioAbastecimento(void);
+
+  /** Retorna o horário do abastecimento
+   * @param - void
+   * @return - String horario: horário do abastecimento
+   */
+  String getHorario(void);
+
+  /** Retorna a data do abastecimento
+   * @param - void
+   * @return - String data: horário do abastecimento
+   */
+  String getData(void);
+
+private:
+  RTC_DS1307 _rtc;
+  String _horario;
+  String _data;
 };
 
 #endif
