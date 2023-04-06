8fafb30b15d615c0ec740a68222b10a3bc094683
LGustta13 <luisgustavoolivtp@hotmail.com>
2023-04-06 08:26:10 -0300

    Salvando e lendo um abastecimento do microSD

--- libs/MicroSdServices/examples/ArduinoJSON/ArduinoJSON.ino
+++ libs/MicroSdServices/examples/ArduinoJSON/ArduinoJSON.ino
@@ -2,14 +2,20 @@
 #include <SD.h>
 #include <SPI.h>
 
+struct Data{
+  char dia[20];
+  int timezone_tipo;
+  char timezone[3];
+};
+
 struct Abastecimento{
   int id;
   char nome_bomba[25];
   float latitude;
   float longitude;
   float volume_abastecido;
-  char data_inicial[20];
-  char data_final[20];
+  Data data_inicial;
+  Data data_final;
   int id_frentista;
   int id_motorista;
   int id_veiculo;
@@ -17,25 +23,35 @@ struct Abastecimento{
 
 const char *filename = "/Abastecimentos.txt";
 Abastecimento abastecimento; 
+Data data;
 
 void loadAbastecimento(const char *filename, Abastecimento &abastecimento){
   File file = SD.open(filename);
-  StaticJsonDocument<768> root;
+  StaticJsonDocument<1024> root;
 
   DeserializationError error = deserializeJson(root, file);
   if (error)
     Serial.println(F("Failed to read file, using default configuration"));
     
-  abastecimento.id = root["tank_id"];
-  strlcpy(abastecimento.nome_bomba, root["tank_name"], sizeof(abastecimento.nome_bomba));
-  abastecimento.latitude = root["initial_volume"];
-  abastecimento.longitude = root["final_volume"];
-  abastecimento.volume_abastecido = root["drain_value"];
-  strlcpy(abastecimento.data_inicial, root["utc_initial_date_time"], sizeof(abastecimento.data_inicial));
-  strlcpy(abastecimento.data_final, root["utc_final_date_time"], sizeof(abastecimento.data_final));
-  abastecimento.id_frentista = root["ident_pessoa_apoio"];
-  abastecimento.id_veiculo = root["ident_veiculo"];
-  abastecimento.id_motorista = root["ident_motorista"];
+  abastecimento.id = root["tank_id"] | 1;
+  strlcpy(abastecimento.nome_bomba, root["tank_name"] | "BOMBA_1", sizeof(abastecimento.nome_bomba));
+  abastecimento.latitude = root["initial_volume"] | 199.99;
+  abastecimento.longitude = root["final_volume"] | 199.99;
+  abastecimento.volume_abastecido = root["drain_value"] | 500.00;
+
+  JsonObject utc_initial_date_time = root["utc_initial_date_time"];
+  strlcpy(abastecimento.data_inicial.dia, utc_initial_date_time["date"] | "2023-01-01 10:00:00", sizeof(abastecimento.data_inicial.dia));
+  abastecimento.data_inicial.timezone_tipo = utc_initial_date_time["timezone_type"] | 3;
+  strlcpy(abastecimento.data_inicial.timezone, utc_initial_date_time["timezone"] | "UTC", sizeof(abastecimento.data_inicial.timezone));
+
+  JsonObject utc_final_date_time = root["utc_final_date_time"];
+  strlcpy(abastecimento.data_final.dia, utc_final_date_time["date"] | "2023-01-01 10:10:00", sizeof(abastecimento.data_final.dia));
+  abastecimento.data_final.timezone_tipo = utc_final_date_time["timezone_type"] | 3;
+  strlcpy(abastecimento.data_final.timezone, utc_final_date_time["timezone"] | "UTC", sizeof(abastecimento.data_final.timezone));
+
+  abastecimento.id_frentista = root["ident_pessoa_apoio"] | 16651608;
+  abastecimento.id_veiculo = root["ident_veiculo"] | 16651608;
+  abastecimento.id_motorista = root["ident_motorista"] | 16651608;
 
   file.close();
 }
@@ -56,8 +72,17 @@ void saveAbastecimento(const char *filename, const Abastecimento &abastecimento)
   root["initial_volume"] = abastecimento.latitude;
   root["final_volume"] = abastecimento.longitude;
   root["drain_value"] = abastecimento.volume_abastecido;
-  root["utc_initial_date_time"] = abastecimento.data_inicial;
-  root["utc_final_date_time"] = abastecimento.data_final;
+
+  JsonObject utc_initial_date_time = root.createNestedObject("utc_initial_date_time");
+  utc_initial_date_time["date"] = abastecimento.data_inicial.dia;
+  utc_initial_date_time["timezone_type"] = abastecimento.data_inicial.timezone_tipo;
+  utc_initial_date_time["timezone"] = abastecimento.data_inicial.timezone;
+  
+  JsonObject utc_final_date_time = root.createNestedObject("utc_final_date_time");
+  utc_final_date_time["date"] = abastecimento.data_final.dia;
+  utc_final_date_time["timezone_type"] = abastecimento.data_final.timezone_tipo;
+  utc_final_date_time["timezone"] = abastecimento.data_final.timezone;
+
   root["ident_pessoa_apoio"] = abastecimento.id_frentista;
   root["ident_veiculo"] = abastecimento.id_veiculo;
   root["ident_motorista"] = abastecimento.id_motorista;
