edd8240f315ca54b738cc26c07e0204f35e07662
Gustavo <luis.oliveira@technolog.com.br>
2024-01-26 11:43:55 -0300

    Teste do datepicker

--- web-vite/src/pages/Dashboard/index.tsx
+++ web-vite/src/pages/Dashboard/index.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import './styles.css'
+
 export default function Dash() {
+    const [startDate, setStartDate] = useState<Date | null>(new Date());
 
     return (
-        <>
-        </>
+
+        <section>
+            <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                locale="pt-BR"
+                showTimeSelect
+                timeFormat="p"
+                timeIntervals={15}
+                dateFormat="Pp"
+            />
+        </section>
+
+
     )
 }
